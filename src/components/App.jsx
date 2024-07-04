1b591173d4a43949c4379c270ccaa92cccf05c5a
AndreyTara <explorituse@gmail.com>
2024-07-05 00:15:04 +0300

    'adds-location'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import Description from "./components/Description/Description";
-import Feedback from "./components/Feedback/Feedback";
-import Options from "./components/Options/Options";
+import Description from "./Description/Description";
+import Feedback from "./Feedback/Feedback";
+import Options from "./Options/Options";
 import "./App.css";
 
 function App() {
