d7bc2808defc2445268e9bd3dc2d3466278b7ac4
AndreyTara <explorituse@gmail.com>
2024-07-05 00:00:25 +0300

    'adds-first-step'

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,5 +1,36 @@
 import css from "./Feedback.module.css";
+import { useState, useEffect } from "react";
+function Feedback({ value }) {
+  const [total, setTotal] = useState(1);
+  const [positive, setPositive] = useState(0);
 
-function Feedback() {}
+  const keysArrValue = Object.keys(value);
+  useEffect(() => {
+    const totalResult = keysArrValue.reduce((acc, item) => {
+      return (acc += value[item]);
+    }, 0);
+    setTotal(totalResult);
+    const positiveCount = keysArrValue
+      .filter((item) => item !== "bad")
+      .reduce((acc, item) => {
+        return (acc += value[item]);
+      }, 0);
+    setPositive(Math.floor((positiveCount / totalResult) * 100));
+  }, [value, total]);
+
+  return (
+    <div>
+      {keysArrValue.map((item) => {
+        return (
+          <p key={item} className={css.p}>
+            {item}: {value[item]}
+          </p>
+        );
+      })}
+      {/* <p className={css.p}>Total: {total}</p>
+      <p className={css.p}>Positive: {positive}%</p> */}
+    </div>
+  );
+}
 
 export default Feedback;
